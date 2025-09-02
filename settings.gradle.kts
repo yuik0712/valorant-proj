faedb3da969097a8c3c8c4df8a5c6fa5f9e91ea8
yuik <163374559+yuik0712@users.noreply.github.com>
2025-09-02 22:20:27 +0900

    Add files via upload

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,9 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
-rootProject.name = "proj"
+rootProject.name = "team-member-system"
+
+// 모든 서브 모듈 포함
+include("common")
+include("core")
+include("member-service")
+include("team-service")
+include("gateway")
+include("common")
