57591e1a988f142964958614d33a41daad278c2e
yuik <163374559+yuik0712@users.noreply.github.com>
2025-09-03 13:34:28 +0900

    [feat] config 패키지 추가

---
+++ common/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("java")
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    id("io.spring.dependency-management")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.boot:spring-boot-dependencies:3.3.2") // Spring Boot 버전 명시
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    // Redis
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+    // Lombok
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
