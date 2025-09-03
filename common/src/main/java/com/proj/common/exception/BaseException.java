fbf415c947767ae511247cdbc6b28a628e687dfd
yuik <163374559+yuik0712@users.noreply.github.com>
2025-09-03 22:27:11 +0900

    [feat] 예외 처리 코드 구현한 커밋

---
+++ common/src/main/java/com/proj/common/exception/BaseException.java
@@ -0,0 +1,20 @@
+package com.proj.common.exception;
+
+// 모든 커스텀 예외 기본 클래스
+public abstract class BaseException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public BaseException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public BaseException(ErrorCode errorCode, String customMessage) {
+        super(customMessage);
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
